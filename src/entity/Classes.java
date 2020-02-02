c17c7a319fd52183a777c97c47b1d676b419cbe4
Jammersg <james.alexander.gallaher@gmail.com>
2020-02-01 23:12:26 -0700

    Classes and Dao

--- src/entity/Classes.java
+++ src/entity/Classes.java
@@ -2,4 +2,17 @@ package entity;
 
 public class Classes {
 
+	private int classId;
+	private String classType;
+	private dateAndTime;
+	private List<Clients> clients;
+	
+	public Classes(int classId, String clssType, List<Clients> clients) {
+		this.classId = classId;
+		this.classType = classType;
+		this.clients = clients;
+		
+	}
+	
+	
 }
